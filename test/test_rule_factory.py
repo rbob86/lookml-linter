34092db064ae6c65f5ddfc3c3675ae0846ee1f8f
Ryan Bobrowski <rbobrowski@google.com>
2022-05-12 09:37:34 -0500

    Moved rule_names get method into rules_engine for consolidation purposes

--- test/test_rule_factory.py
+++ test/test_rule_factory.py
@@ -1,12 +1,12 @@
 import pytest
 from linter.severity import Severity
 from linter.rule_factory import RuleFactory
-from linter.rule_names import RuleNames
+from linter.rules_engine import RulesEngine
 
 
 def test_build_method_throws_key_error_if_rule_class_does_not_exist() -> None:
     factory = RuleFactory()
-    for rule_name in RuleNames.get():
+    for rule_name in RulesEngine.rule_names():
         rule_name = rule_name + '--invalid'
         with pytest.raises(KeyError):
             factory.build(rule_name)
@@ -14,7 +14,7 @@ def test_build_method_throws_key_error_if_rule_class_does_not_exist() -> None:
 
 def test_build_method_returns_appropriate_rule() -> None:
     factory = RuleFactory()
-    for rule_name in RuleNames.get():
+    for rule_name in RulesEngine.rule_names():
         rule = factory.build(rule_name)
         rule_class_name = type(rule).__name__
         class_name = factory._RuleFactory__rule_name_to_classname(rule_name)
@@ -23,7 +23,7 @@ def test_build_method_returns_appropriate_rule() -> None:
 
 def test_build_method_returns_rule_with_correct_severity() -> None:
     factory = RuleFactory()
-    for rule_name in RuleNames.get():
+    for rule_name in RulesEngine.rule_names():
         rule = factory.build(rule_name)
         rule_class = rule.__class__
         assert rule.severity == rule_class.default_severity()
