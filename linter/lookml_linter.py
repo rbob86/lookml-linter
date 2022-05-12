8998473ff15eaa713ad8dc1de831a1cd4d422246
Ryan Bobrowski <rbobrowski@google.com>
2022-05-12 09:29:14 -0500

    Loop through each file before linting each object

--- linter/lookml_linter.py
+++ linter/lookml_linter.py
@@ -1,6 +1,7 @@
 
 from typing import Dict, List
 from linter.rule import Rule
+from linter.severity import Severity
 
 
 class LookMlLinter:
@@ -9,23 +10,26 @@ class LookMlLinter:
         self.rules = rules
 
     def run(self) -> None:
-        views = self.data.get('views', [])
-        explores = self.data.get('explores', [])
+        for file in self.data:
+            file_data = self.data[file]
+            views = file_data.get('views', [])
+            explores = file_data.get('explores', [])
 
-        for v in views:
-            self.__lint_object(v, 'view')
-            dimensions, measures, dimension_groups = [
-                v.get(key, []) for key in ['dimensions', 'measures', 'dimension_groups']]
-            for d in dimensions:
-                self.__lint_object(d, 'dimension')
-            for g in dimension_groups:
-                self.__lint_object(g, 'dimension_group')
-            for m in measures:
-                self.__lint_object(m, 'measure')
+            for v in views:
+                self.__lint_object(v, 'view')
+                dimensions, measures, dimension_groups = [
+                    v.get(key, []) for key in ['dimensions', 'measures', 'dimension_groups']]
+                for d in dimensions:
+                    self.__lint_object(d, 'dimension')
+                for g in dimension_groups:
+                    self.__lint_object(g, 'dimension_group')
+                for m in measures:
+                    self.__lint_object(m, 'measure')
 
-        for e in explores:
-            self.__lint_object(e, 'explore')
+            for e in explores:
+                self.__lint_object(e, 'explore')
 
     def __lint_object(self, object: Dict, object_type: str) -> None:
-        for rule in self.rules[object_type]:
-            rule.run(object)
+        if object_type in self.rules:
+            for rule in self.rules[object_type]:
+                rule.run(object)
