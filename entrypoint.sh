6e81b1691c2ad12916529e21175a10dd917bff70
jdemartino78 <47394380+jdemartino78@users.noreply.github.com>
2022-05-28 00:25:19 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
-find ./ -type d
+ALPINE_VERSION=$1
 
-find ./ -type f 
+docker build -t docker-action \
+             -f ./docker-action/Dockerfile \
+             --build-arg alpine_version="$ALPINE_VERSION" \
+             ./docker-action
+
+PROJECT_NAME="$(basename ${GITHUB_REPOSITORY})"
+WORKSPACE="${RUNNER_WORKSPACE}/${PROJECT_NAME}"
+
+docker run --workdir=$GITHUB_WORKSPACE \
+           -v $WORKSPACE:$GITHUB_WORKSPACE \
+           docker-action "$@"
 
 
-python -m linter.main config.example.yaml
+# python -m linter.main config.example.yaml
