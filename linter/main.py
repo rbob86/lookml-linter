da88bda517eb0c3eab83fb70a2f0b620da795a4d
eric-lyons <eric.lyons@looker.com>
2022-06-01 16:13:43 +0000

    added arg parse and vars

--- linter/main.py
+++ linter/main.py
@@ -1,21 +1,32 @@
 import sys
+import argparse
 from linter.config_validator import ConfigValidator
 from linter.lookml_linter import LookMlLinter
 from linter.rules_engine import RulesEngine
 from linter.lookml_project_parser import LookMlProjectParser
 
 
-def main():
-    args = sys.argv[1:]
 
-    config_file = None
-    if len(args) > 0:
-        config_file = args[0]
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Args for LookML linter.')
+    parser.add_argument('--configFile', type=str,
+                    help='file path for configuartion settings file')
+    parser.add_argument('--lookmlProjectPath', type=str,
+                    help='path for the lookml files')
+
+    args = parser.parse_args()
+
+    config_file = args.configFile
+    path = args.lookmlProjectPath
+    
 
     validator = ConfigValidator(config_file)
     validator.validate()
     config = validator.config
     rules = RulesEngine(config).rules
+    LookMlProjectParser.root_file_path = path
     data = LookMlProjectParser().get_parsed_lookml_files()
     linter = LookMlLinter(data, rules)
     linter.run()
