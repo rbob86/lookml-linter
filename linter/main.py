da95b9fed3a8084d60d01db7220ab3f3c20b6fe2
jdemartino78 <47394380+jdemartino78@users.noreply.github.com>
2022-06-02 16:14:47 -0400

    Update main.py

--- linter/main.py
+++ linter/main.py
@@ -9,14 +9,14 @@ from linter.lookml_project_parser import LookMlProjectParser
 
 def main():
     config_file = os.environ['INPUT_CONFIGFILE']
-    path = os.environ['INPUT_LOOKMLPROJECT']
+    #path = os.environ['INPUT_LOOKMLPROJECT']
     
 
     validator = ConfigValidator(config_file)
     validator.validate()
     config = validator.config
     rules = RulesEngine(config).rules
-    LookMlProjectParser.root_file_path = path
+    #LookMlProjectParser.root_file_path = path
     data = LookMlProjectParser().get_parsed_lookml_files()
     linter = LookMlLinter(data, rules)
     linter.run()
