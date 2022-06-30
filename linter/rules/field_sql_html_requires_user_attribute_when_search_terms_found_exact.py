d34de260e318a93ef512786423cdb09ed4f5001d
Rudo Terry <rudo.terry@ro.co>
2022-06-30 18:45:51 +0100

    Deleting partial match rule. Adding rule to match column names exactly

--- linter/rules/field_sql_html_requires_user_attribute_when_search_terms_found_exact.py
+++ linter/rules/field_sql_html_requires_user_attribute_when_search_terms_found_exact.py
@@ -1,7 +1,7 @@
 from linter.rule import Rule
 
 
-class FieldSqlHtmlRequiresUserAttributeWhenSearchTermsFound(Rule):
+class FieldSqlHtmlRequiresUserAttributeWhenSearchTermsFoundExact(Rule):
     def applies_to():
         return ('dimension', 'measure', 'dimension_group')
 
@@ -10,9 +10,9 @@ class FieldSqlHtmlRequiresUserAttributeWhenSearchTermsFound(Rule):
         search_terms = self.params['search_terms']
         search_pattern = ' '.join([field.get(key, '')
                                    for key in ['name', 'label', 'description']])
-        user_attribute_search_term = f'_user_attributes[""{user_attribute}""]'
+        user_attribute_search_term = f'_user_attributes["{user_attribute}"]'
         search_terms_match_in_name_label_description = any(
-            [word in search_pattern for word in search_terms])
+            [word == search_pattern.strip() for word in search_terms])
         user_attribute_check_in_sql_html = not all([user_attribute_search_term in string for
                                                     string in [field.get('sql', ''), field.get('html', '')]])
         if search_terms_match_in_name_label_description and user_attribute_check_in_sql_html:
