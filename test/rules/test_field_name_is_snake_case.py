41c51744daa71dc077e99df8a5bfbef2ef73f5c8
Vishal Dharmadhikari <dharmadhikari@google.com>
2023-02-08 02:36:09 +0000

    Update file and class name for field_name_is_snake_case.

--- test/rules/test_field_name_is_snake_case.py
+++ test/rules/test_field_name_is_snake_case.py
@@ -1,9 +1,9 @@
 from linter.rule import Severity
-from linter.rules.field_is_snake_case import FieldIsSnakeCase
+from linter.rules.field_name_is_snake_case import FieldNameIsSnakeCase
 
 
-def test_run_method_successfully_validates_field_is_snake_case() -> None:
-    rule = FieldIsSnakeCase(Severity.ERROR.value)
+def test_run_method_successfully_validates_field_name_is_snake_case() -> None:
+    rule = FieldNameIsSnakeCase(Severity.ERROR.value)
 
     field = {
         "name": "order_id",
@@ -15,14 +15,14 @@ def test_run_method_successfully_validates_field_is_snake_case() -> None:
     assert rule_result == True
 
 
-def test_run_method_fails_with_field_is_not_snake_case() -> None:
-    rule = FieldIsSnakeCase(Severity.ERROR.value)
+def test_run_method_fails_field_name_is_not_snake_case() -> None:
+    rule = FieldNameIsSnakeCase(Severity.ERROR.value)
 
     field = {
         "name": "OrderID",
         "primary_key": "yes",
         "sql": '${TABLE}."ORDER_ID"',
         "type": "number",
-    }    
+    }
     rule_result = rule.run(field)
     assert rule_result == False
