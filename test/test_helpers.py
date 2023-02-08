f33a2e39b8824052ec1a11b449aa4631765f208e
Vishal Dharmadhikari <dharmadhikari@google.com>
2023-02-08 02:42:08 +0000

    Add test for is_snake_case helper function

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -1,33 +1,37 @@
-from linter.helpers import pascal_case_to_snake_case, snake_case_to_pascal_case
+from linter.helpers import (
+    pascal_case_to_snake_case,
+    snake_case_to_pascal_case,
+    is_snake_case,
+)
 
 
 def test_pascal_case_to_snake_case_successfully_converts_pascal_to_snake() -> None:
-    input = 'TestPascalStr'
-    expected_output = 'test_pascal_str'
+    input = "TestPascalStr"
+    expected_output = "test_pascal_str"
     assert pascal_case_to_snake_case(input) == expected_output
 
 
 def test_pascal_case_to_snake_case_successfully_converts_snake_to_snake() -> None:
-    input = 'test_snake_str'
-    expected_output = 'test_snake_str'
+    input = "test_snake_str"
+    expected_output = "test_snake_str"
     assert pascal_case_to_snake_case(input) == expected_output
 
 
 def test_pascal_case_to_snake_case_successfully_converts_hybrid_to_snake() -> None:
-    input = 'test_hybridStr'
-    expected_output = 'test_hybrid_str'
+    input = "test_hybridStr"
+    expected_output = "test_hybrid_str"
     assert pascal_case_to_snake_case(input) == expected_output
 
 
 def test_pascal_case_to_snake_case_does_not_strip_symbols() -> None:
-    input = 'test_snake_str***'
-    expected_output = 'test_snake_str***'
+    input = "test_snake_str***"
+    expected_output = "test_snake_str***"
     assert pascal_case_to_snake_case(input) == expected_output
 
 
 def test_snake_case_to_pascal_case_successfully_converts_snake_to_pascal() -> None:
-    input = 'test_snake_str'
-    expected_output = 'TestSnakeStr'
+    input = "test_snake_str"
+    expected_output = "TestSnakeStr"
     assert snake_case_to_pascal_case(input) == expected_output
 
 
@@ -47,3 +51,13 @@ def test_snake_case_to_pascal_case_successfully_converts_snake_to_pascal() -> No
 #     input = 'TestPascalStr***'
 #     expected_output = 'TestPascalStr***'
 #     assert snake_case_to_pascal_case(input) == expected_output
+
+
+def test_is_snake_case_validates_successfully() -> None:
+    input = "this_is_snake_case"
+    assert is_snake_case(input) == True
+
+
+def test_is_snake_case_fails() -> None:
+    input = "ThisIsNotSnakeCase"
+    assert is_snake_case(input) == False
