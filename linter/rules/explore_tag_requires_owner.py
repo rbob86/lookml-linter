fea4205afb34aa556fe9062d86823be8a69e1dc9
Vishal Dharmadhikari <dharmadhikari@google.com>
2023-02-14 16:11:56 +0000

    Fix: update order of rules in config.example.yaml. Clean up explore_tag_requires_owner rule.

--- linter/rules/explore_tag_requires_owner.py
+++ linter/rules/explore_tag_requires_owner.py
@@ -10,8 +10,7 @@ class ExploreTagRequiresOwner(Rule):
     def run(self, explore: Any) -> bool:
         if not 'tags' in explore:
             return False
-        else:
-            tags = explore.get('tags')
-            pattern = compile(r'^owner:')
-            tag_contains_owner = [bool(pattern.match(tag)) for tag in tags]
-            return any(tag_contains_owner)
+        tags = explore.get('tags')
+        pattern = compile(r'^owner:')
+        tag_contains_owner = [bool(pattern.match(tag)) for tag in tags]
+        return any(tag_contains_owner)
