e128f805b8318864b68c7b7808570a728b0fa5d9
rbob86 <rbobrowski@google.com>
2023-03-21 13:24:44 -0400

    Testing new flag for saving output to file

--- linter/main.py
+++ linter/main.py
@@ -10,6 +10,7 @@ def main():
     config_file = os.environ['INPUT_CONFIGFILE']
     filepaths = os.environ['INPUT_FILEPATHS']
     filepaths = filepaths.split(' ') if filepaths != None else None
+    save_output_to_file = os.environ['INPUT_SAVEOUTPUTTOFILE']
 
     # Validate config.yaml file
     validator = ConfigValidator(config_file)
@@ -20,12 +21,15 @@ def main():
     data = LookMlProjectParser(filepaths).get_parsed_lookml_files()
 
     if data:
-        # Run linter and save/print output
+        # Run linter and print output
         linter = LookMlLinter(data, rules)
         linter.run()
         error_log = linter.get_errors()
         print(error_log)
-        linter.save_errors(error_log, '_lookml-linter-output.txt')
+
+        # Save output to file, if enabled
+        if save_output_to_file != 'false':
+            linter.save_errors(error_log, '_lookml-linter-output.txt')
 
         # Fail GitHub Action only if linter has errors (warnings do not count)
         assert linter.has_errors == False, 'LookML Linter detected an error.'
